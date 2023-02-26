7e74d7220686fda8d4ce70d5ac009960f93ffe0c
Leandro Quipildor <83482016+LeooQuipildor@users.noreply.github.com>
2023-02-26 18:22:24 -0300

    Primer commit agregue los ejercicio 1,2,3,4 y 5 todos bien realizados

---
+++ Ejercicio5.js
@@ -0,0 +1,3 @@
+let numero1 = parseInt(prompt("Ingrese un numero"));
+let numero2 = parseInt(prompt("Ingrese otro numero"));
+document.write("La suma de " + numero1 + " y "+ numero2+ " es = " + (numero1 + numero2));
