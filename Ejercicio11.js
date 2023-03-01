ee07f5bbc87817b3ed744e940f3d9570936bef7f
Leandro Quipildor <83482016+LeooQuipildor@users.noreply.github.com>
2023-02-28 21:39:16 -0300

    Agregue el ejercicio 11

--- Ejercicio11.js
+++ Ejercicio11.js
@@ -8,9 +8,21 @@ let divisiblePor7 = numero % 7 === 0;
 
 if (!isNaN(numero)) {
     if (divisiblePor2 || divisiblePor3 || divisiblePor5 || divisiblePor7) {
-        document.write(`El numero (${numero}) es divisible por 2, 3, 5 o 7`)
+        if (divisiblePor2) {
+            mensaje += " 2";
+        }
+        if (divisiblePor3) {
+            mensaje += ", 3";
+        }
+        if (divisiblePor5) {
+            mensaje += ", 5";
+        }
+        if (divisiblePor7) {
+            mensaje += " y 7";
+        }
+        document.write(mensaje);
     } else {
-        document.write(`El número ${numero} NO es divisible por 2, 3, 5 ni 7`);
+        document.write(`El número ${numero} no es divisible por 2, 3, 5 ni 7`);
     }
 }
 else {
